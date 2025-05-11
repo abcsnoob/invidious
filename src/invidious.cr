a6c7cfa531064ec31ddc649a5ac68a29b1fac075
Abc's Noob <abcsspprt@gmail.com>
2025-05-11 15:32:20 +0700

    Update invidious.cr

--- src/invidious.cr
+++ src/invidious.cr
@@ -76,14 +76,11 @@ REQUEST_HEADERS_WHITELIST  = {"accept", "accept-encoding", "cache-control", "con
 RESPONSE_HEADERS_BLACKLIST = {"access-control-allow-origin", "alt-svc", "server"}
 HTTP_CHUNK_SIZE            = 10485760 # ~10MB
 
-CURRENT_BRANCH  = {{ "#{`git branch | sed -n '/* /s///p'`.strip}" }}
-CURRENT_COMMIT  = {{ "#{`git rev-list HEAD --max-count=1 --abbrev-commit`.strip}" }}
-CURRENT_VERSION = {{ "#{`git log -1 --format=%ci | awk '{print $1}' | sed s/-/./g`.strip}" }}
+CURRENT_BRANCH  = "master"
+CURRENT_COMMIT  = "dummy_commit_id"
+CURRENT_VERSION = "2025.05.11"
+ASSET_COMMIT = "dummy_asset_commit_id"
 
-# This is used to determine the `?v=` on the end of file URLs (for cache busting). We
-# only need to expire modified assets, so we can use this to find the last commit that changes
-# any assets
-ASSET_COMMIT = {{ "#{`git rev-list HEAD --max-count=1 --abbrev-commit -- assets`.strip}" }}
 
 SOFTWARE = {
   "name"    => "invidious",
